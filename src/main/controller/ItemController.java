c3791618efcb0df42245d8a35fcedc8cc8322ccc
Paweł Przystarz <gienasd@gmail.com>
2024-02-01 17:37:23 +0100

    2 task
    add 2nd task

--- src/main/controller/ItemController.java
+++ src/main/controller/ItemController.java
@@ -9,5 +9,29 @@ public class ItemController {
         // all items from Ikea
         System.out.println("Items which you will buy in Ikea: "
                 + itemService.filterByIkeaList());
+
+        // all items in kitchen
+        System.out.println("Items which you will keep in Kitchen: "
+                + itemService.filterByKitchenList());
+
+        // cost is more than 1000 PLN
+        System.out.println("Items which you will buy for more than 1000PL: "
+                + itemService.filterByPriceMoreThan(1000d));
+
+        //Cost in Ikea
+        System.out.println("In Ikea you will spend: "
+                + itemService.getCostOfItemsFromIkea());
+
+        // tools from Castorama
+        System.out.println("Tools which you will buy in Castorama: "
+                + itemService.filterByCastoramaList());
+
+        // more than one piece of item
+        System.out.println("Items which you will buy more than one piece: "
+                + itemService.filterByCountMoreThanOneList());
+
+        // else items with we will buy
+        System.out.println("Rest of the items: " + itemService.getRestList());
+
     }
 }
--- src/main/service/ItemService.java
+++ src/main/service/ItemService.java
@@ -2,7 +2,11 @@ package service;
 
 import dao.ItemDao;
 import entities.Item;
+import entities.KindOfItem;
+import entities.Room;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class ItemService {
@@ -12,5 +16,53 @@ public class ItemService {
         return itemDao.getShopList().stream()
                 .filter(item -> item.getShop().equalsIgnoreCase("Ikea"))
                 .toList();
+    }
+
+    public List<Item> filterByKitchenList() {
+        return itemDao.getShopList().stream()
+                .filter(item -> Room.KITCHEN.equals(item.getRoom()))
+                .toList();
+    }
+
+    public List<Item> filterByPriceMoreThan(double price) {
+        return itemDao.getShopList().stream()
+                .filter(item -> item.getCost() > price)
+                .toList();
+    }
+
+    public double getCostOfItemsFromIkea() {
+        return itemDao.getShopList().stream()
+                .filter(item -> item.getShop().equalsIgnoreCase("Ikea"))
+                .mapToDouble(Item::getCost)
+                .sum();
+    }
+
+    public List<Item> filterByCastoramaList() {
+        return itemDao.getShopList().stream()
+                .filter(item -> item.getShop().equalsIgnoreCase("Castorama"))
+                .filter(item -> KindOfItem.TOOL.equals(item.getKindOfItem()))
+                .toList();
+    }
+
+    public List<Item> filterByCountMoreThanOneList() {
+        return itemDao.getShopList().stream()
+                .filter(item -> item.getCount() > 1)
+                .toList();
+    }
+
+    public List<Item> getRestList() {
+        List<Item> restList = new ArrayList<>(itemDao.getShopList());
+        restList.removeAll(getAllFilteredItems());
+        return restList;
+
+    }
+
+    private HashSet<Item> getAllFilteredItems() {
+        HashSet<Item> allUsedItems = new HashSet<>();
+        allUsedItems.addAll(filterByIkeaList());
+        allUsedItems.addAll(filterByCountMoreThanOneList());
+        allUsedItems.addAll(filterByCastoramaList());
+        allUsedItems.addAll(filterByKitchenList());
+        return allUsedItems;
     }
 }
